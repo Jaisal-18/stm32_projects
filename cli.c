3b5542963ac5072e5ebb36ed8677fd2bb81ff22f
Jaisal-18 <jaisalibhrahim05@gmail.com>
2024-12-27 14:52:34 +0530

    Add files via upload
    clin.c updated

--- cli.c
+++ cli.c
@@ -1,13 +1,45 @@
 #include "gpio.h"
 #include "serial.h"
+#include <string.h>
+char buff[16];
+unsigned short int index=0;
 void cln_fun(void)
 {
 unsigned char data;
 	while(1)
 	{
 		data=serial_getc();
-		gpio_led_on();
 		serial_putc(data);
-		gpio_led_off();
+		if(data=='\n'||data=='\r')
+		{
+			buff[index+1]='\0';
+			serial_putc('\n');
+			serial_putc('\r');
+			for(int i=0;i<index;i++)
+			{
+				serial_putc(buff[i]);
+			}
+			memset(buff,'\0',sizeof(buff));
+			index=0;
+			serial_putc('\n');
+			serial_putc('\r');
+		}
+		else
+		{
+			buff[index++]=data;
+			if(index>=sizeof(buff))
+			{
+				 serial_putc('\n');
+			   serial_putc('\r');
+				 serial_putc('f');
+				 serial_putc('u');
+				 serial_putc('l');
+				 serial_putc('l');
+				 memset(buff,'\0',sizeof(buff));
+				 index=0;
+				 serial_putc('\n');
+				 serial_putc('\r');
+			}
+		}
 	}
 }
